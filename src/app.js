7c536a59dbea698b4138131e9428a9ad49b78311
HansFigo <hambasahaya0303@gmail.com>
2024-01-17 11:50:47 +0700

    chore: update project setup and documentation files
    - Update file `.dockerignore` to change file mode from 100755 to 100644
    - Update file `.env.example` to change file mode from 100755 to 100644
    - Update file `.gitignore` to change file mode from 100755 to 100644
    - Update file `.prettierrc` to change file mode from 100755 to 100644
    - Update file `Dockerfile` to change file mode from 100755 to 100644
    - Update file `README.md` to change file mode from 100755 to 100644
    - Add new file `src/contact.js` to store contact data
    - Update file `src/app.js` to add routes for getting all contacts and creating a new contact
    - Update file `package.json` to change file mode from 100755 to 100644
    
    feat: add support for getting all contacts and creating a new contact
    
    - Add route `/contacts` to get all contacts from the `CONTACTS` variable
    - Add route `/contacts` to create a new contact and store it in the `CONTACTS` variable
    - Validate request body to ensure required fields are present before creating a new contact
    - Return appropriate response messages and data for successful and failed contact creation

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
-require('express-async-errors');
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const { CONTACTS } = require('./contact');
 
 const port = process.env.PORT || 3030;
 const host = process.env.HOST || 'localhost';
@@ -14,6 +14,40 @@ app.use(express.urlencoded({ extended: false }));
 // routes
 app.get('/hello', (req, res) => {
     res.json({ message: 'Hello world' });
+});
+
+// get all contacts
+app.get('/contacts', (req, res) => {
+    // TODO: ambil semua data kontak dari variabel CONTACTS
+    res.json({ status: 'Success', data: CONTACTS });
+});
+
+// create contact
+app.post('/contacts', (req, res) => {
+    // get data from request body
+    const { full_name, phone_number, email } = req.body;
+    // TODO: simpan data dari request body kedalam variabel CONTACTS
+
+    if (!full_name || !phone_number, !email) {
+        res.json({ status: 'error', message: 'Error data kosong', data: CONTACTS });
+
+        throw error
+    }
+
+    console.log(
+        req.body
+    );
+
+    const newContact = {
+        id: (CONTACTS.length + 1).toString(),
+        full_name: full_name,
+        email: email,
+        phone_number: phone_number.toString(),
+    }
+
+    CONTACTS.push(newContact)
+
+    res.json({ status: 'Success', message: 'Contact created', data: newContact });
 });
 
 // 404 endpoint middleware
